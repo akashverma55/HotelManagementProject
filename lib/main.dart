10031067492af9dba21160a50f8e0c288fa0d8a9
Hariprakassh <2k21cse041@kiot.ac.in>
2023-08-31 23:49:46 +0530

    Hariprakassh

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hotel_management_app/screens/LoginScreen.dart';
+import 'package:hotel_management_app/started.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,7 +16,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen()
+      home: started()
     );
   }
 }
